1f33c9bfc8b2a89d5f20b54ca8c936a80333e731
Scott Baitz <scott.w.baitz@gmail.com>
2023-03-20 16:57:25 -0600

    Explain the goal of the challenge, and pseudocode

--- string_of_4.rb
+++ string_of_4.rb
@@ -1,9 +1,11 @@
-# 1. Given an array of strings, return only the strings that have exactly 4 characters.
-
 # For each challenge you complete, include comments showing the **overall goal** in your own words, your **pseudocode**, and your **final solution**. 
 
+# The overall goal of this function is to add the words with a length of 4 to an array titled length_of_four which can then be called later
+
+# Set an array named words, and fill it with randomly generated words. Then call an empty array called length_of_four to store only the words with the length of 4 characters. Iterate through the words array, and if the word has a length of four, append that to the length of four array. Then print the length_of_four array to see if the words were pushed into the array.
 
 words = ["weed", "hypothesis", "explode", "aquarium", "shell", "useful", "registration", "pit", "civilian", "improvement", "circle", "read", "initial", "slot", "global", "answer", "resist", "girlfriend", "smart", "beautiful"]
+
 length_of_four = []
 
 words.each do |word|
