33ea4cd7aab228d2ca1721f13361898174f4c6d1
Scott Baitz <scott.w.baitz@gmail.com>
2023-03-20 21:46:21 -0600

    Completed start with t and downcase

--- downcase.rb
+++ downcase.rb
@@ -1,3 +1,14 @@
 # 1. Start with an array of strings with a mix of uppercase and lowercase letters. Print every word in all lowercase letters.
 
 # For each challenge you complete, include comments showing the **overall goal** in your own words, your **pseudocode**, and your **final solution**. 
+
+# So what I want to do here is turn all of these into downcase letters, which I should be able to do by iterating this array, and apply the downcase function to each string then pass it back into a new array which I will call downcased.
+
+words = ["WeED", "HYPotHEsIs", "eXPLoDE", "aQUARIum", "SHelL", "USeFUL", "rEgiStRatiOn", "pIT", "CivIliaN", "IMprovEmEnT", "cIrCLE", "rEad", "InItIAL", "sLoT", "GLObAl", "AnSWEr", "reSiSt", "giRLfRIEnd", "smaRT", "BeaUtIFUL"]
+downcased = []
+
+words.each do |word|
+    downcased.push(word.downcase)
+end
+
+puts downcased
--- start_with_t.rb
+++ start_with_t.rb
@@ -1,3 +1,15 @@
 # 1. Given an array of strings, return only the words that begin with the letter "t".
     
 # For each challenge you complete, include comments showing the **overall goal** in your own words, your **pseudocode**, and your **final solution**. 
+
+words = ["weed", "hypothesis", "explode", "aquarium", "shell", "useful", "registration", "pit", "civilian", "improvement", "circle", "read", "initial", "slot", "global", "answer", "resist", "girlfriend", "smart", "beautiful", "trunk", "terms", "tower", "talkative", "tolerant"]
+
+starts_with_t = []
+
+words.each do |word|
+    if word.start_with?("t")
+        starts_with_t.push(word)
+    end
+end
+
+puts starts_with_t
